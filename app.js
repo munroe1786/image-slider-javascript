01b5bfcacae583544a24ff89ad6a7fa6e090c9e7
munroe1786 <era3munroe@gmail.com>
2021-05-30 20:28:09 -0500

    add consts to app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector(".slider");
+const before = document.querySelector(".eiffel-tower-before");
+const after = document.querySelector(".eiffel-tower-after");
+const container = document.querySelector(".container");
