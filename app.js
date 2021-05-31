50a06e891cde5349413962fbff5400c28cbcdff7
munroe1786 <era3munroe@gmail.com>
2021-05-30 20:53:39 -0500

    add size variable and second if statement to imageSlider function

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const container = document.querySelector(".container");
 
 const imageSlider = (event) => {
     let xPos = event.layerX;
+    let size = container.offsetWidth;
     before.style.width = xPos + "px";
     slider.style.left = xPos + "px";
 
     if(xPos < 30) {
         before.style.width = 0;
         slider.style.left = 0;
+    }
+    if(xPos + 30 > size) {
+        before.style.width = size + "px";
+        slider.style.left = size + "px";
     }
 }
 
